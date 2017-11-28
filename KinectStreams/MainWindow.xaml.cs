9d64cd1f7e614498cd0817d3b7d43cf1c33636f7
Arthur Christoph <achristoph@gmail.com>
2017-11-28 00:35:22 -0600

    init

---
+++ KinectStreams/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectStreams
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Members
+
+        Mode _mode = Mode.Color;
+
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        IList<Body> _bodies;
+
+        bool _displayBody = false;
+        bool initialTrack = true;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            // Color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Color)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Depth
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Depth)
+                    {
+                        var frameData = new ushort[frame.FrameDescription.Width * frame.FrameDescription.Height];
+                        frame.CopyFrameDataToArray(frameData);
+                        
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Infrared
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Infrared)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Body
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    canvas.Children.Clear();
+
+                    _bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(_bodies);
+                    lbl.Content = "1:"+_bodies[0].IsTracked + " | 2:" + _bodies[1].IsTracked + " | 3:"+ _bodies[2].IsTracked + " | 4:" +  _bodies[3].IsTracked + " | 5:" + _bodies[4].IsTracked + " | 6:" + _bodies[5].IsTracked;
+                    
+                    foreach (var body in _bodies)
+                    {
+                        if (body != null)
+                        {
+                            if (body.IsTracked)
+                            {
+
+                                status.Content = string.Join("\n", body.Joints.Select(item => item.Key + ":" + Math.Round(item.Value.Position.X,1) + " " + Math.Round(item.Value.Position.Y,1) + " " + Math.Round(item.Value.Position.Z,1)));
+                               
+                                // Draw skeleton.
+                                if (_displayBody)
+                                {
+                                    canvas.DrawSkeleton(body);
+                                }
+                            }
+                        }
+                    }
+                    if (_bodies.All((b) => !b.IsTracked) && !initialTrack)
+                    {
+                        BitmapImage imgSource = new BitmapImage(new Uri("frog2.jpg", UriKind.Relative));
+                        animation.Source = imgSource;
+                    }
+                    else
+                    {
+                        initialTrack = false;
+                        BitmapImage imgSource = new BitmapImage(new Uri("frog1.jpg", UriKind.Relative));
+                        animation.Source = imgSource;
+                    }
+                }
+            }
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Infrared;
+        }
+
+        private void Body_Click(object sender, RoutedEventArgs e)
+        {
+            _displayBody = !_displayBody;
+        }
+
+        #endregion
+    }
+
+    public enum Mode
+    {
+        Color,
+        Depth,
+        Infrared
+    }
+}
